06b9fd7c17a68697829dc6f1996356eb6a611264
vxsl <kyle.grimsrudmanz@gmail.com>
2020-03-02 12:25:26 -0500

    first version. Partially working to enter albums to the database. Just realized that I confused Moods with Themes on the Allmusic site, so next will be fixing this.

---
+++ app.py
@@ -0,0 +1,158 @@
+from flask import Flask, render_template, url_for, request, redirect
+
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from datetime import datetime
+
+import process.scraper
+import process.updateMoodsDb
+
+from collections import defaultdict
+
+
+#****************************************************
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'alpine'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+#mood_db = SQLAlchemy(app)
+
+admin = Admin(app)
+
+#****************************************************
+#class User(db.Model):
+#	__tablename__ = 'user'
+#	id = db.Column('id', db.String(), primary_key=True)
+	#password = db.Column(db.String())
+
+#****************************************************
+
+class ImprovedModel(ModelView):
+	column_display_pk = True
+
+#****************************************************
+# Album-Mood ASSOCIATION TABLE:
+
+"""class MoodAssociation(db.Model):
+	__tablename__='mood_associations'
+	album_id = db.Column(db.String(), db.ForeignKey('album.id'))
+	mood_name = db.Column(db.String(), db.ForeignKey('mood.name'), primary_key = True)"""
+
+
+"""
+class MoodAssociation(db.Model):
+	__tablename__ = 'mood_associations'
+	album_id = db.Column(db.String())
+	mood_name = db.Column(db.String(), primary_key=True)
+"""
+
+
+class MoodAssociation(db.Model):
+	__tablename__ = 'mood_associations'
+	album_id = db.Column(db.String(), primary_key=True)
+	mood_name = db.Column(db.String(), primary_key=True)
+
+
+mood_associations = db.Table('mood_associations',
+	db.Column('album_id', db.String(), db.ForeignKey('album.id'), primary_key=True),
+	#db.Column('mood_name', db.String(), db.ForeignKey('mood.name'), primary_key=True),
+	
+	db.Column('mood_name', db.String(), db.ForeignKey('mood.name'), primary_key=True),
+	extend_existing=True
+)
+#****************************************************
+
+class Album(db.Model):
+	__tablename__ = 'album'
+	id = db.Column(db.String(), primary_key=True)
+	artist = 		db.Column(db.String())
+	title = 		db.Column(db.String())
+	date_added = 	db.Column(db.DateTime, default=datetime.utcnow)
+
+	
+	#moods = db.relationship('Mood', secondary=mood_associations, back_populates='participants')
+
+	moods = db.relationship('Mood', secondary=mood_associations, backref=(db.backref('participants', lazy = 'dynamic')))
+
+	def __repr__(self):
+		return '<Album %r>' % self.id
+
+#****************************************************
+
+class Mood(db.Model):
+	__tablename__ = 'mood'
+	id = db.Column(db.String(), primary_key=True)
+	name = db.Column(db.String())
+	#participants = db.relationship('Album', secondary=mood_associations, back_populates='moods')
+
+#****************************************************
+
+
+#process.updateMoodsDb.update(db, Mood)
+
+#db.create_all(
+
+admin.add_view(ImprovedModel(Album, db.session))
+admin.add_view(ImprovedModel(Mood, db.session))
+admin.add_view(ImprovedModel(MoodAssociation, db.session))
+
+#moodTemp = Mood.query.filter_by(name='Angry').first()
+#albumTemp = Album.query.filter_by(id='mw0000042674').first()
+
+#moodTemp.participants.append(albumTemp)
+#db.session.commit()
+
+
+#****************************************************
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+	if request.method == 'POST':
+
+		artistInput = request.form['artist']
+		titleInput = request.form['title']
+
+		albumInfo = process.scraper.getBasicInfo(titleInput, artistInput)
+
+		newAlbumEntry = Album(id=albumInfo['id'], artist=albumInfo['artist'], title=albumInfo['title'])
+
+		
+		for n,i in process.scraper.getMoods(albumInfo['id'], db, Mood):
+			print(f'\n\n\n{n}\n\n\n')
+			moodTemp = Mood.query.filter_by(name=f'{n}').first()
+			moodTemp.participants.append(newAlbumEntry)
+	
+		try:
+			db.session.add(newAlbumEntry)
+			db.session.commit()
+			return redirect('/')
+		except:
+			return "There was an error adding your album"
+	else:
+		albumlist = Album.query.order_by(Album.date_added).all()
+
+		#moodDict = {}
+		moodDict = defaultdict(list)
+		
+		for album in albumlist:
+			for association in MoodAssociation.query.filter_by(album_id=album.id).all():
+				moodname = association.mood_name
+
+				#if album.title in moodDict:
+				moodDict[album.title].append(moodname)
+				#else:
+					#moodDict[album.title] = moodname
+
+
+		return render_template('index.html', albumlist=albumlist, moodDict=moodDict)		
+	
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
+ 
---
+++ process/scraper.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3	
+
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+from html.parser import HTMLParser
+import requests
+import re
+
+def progressMessage(msg):
+	print(f"\n......... {msg} ..................\n\n")
+
+#artist = input("Artist: ").replace(" ", "+")
+#album = input("Album: ").replace(" ", "+")
+
+#artist = "Boards of Canada"
+#album = "Music Has the right to children"
+
+#artist = input("Artist: ")
+#album = input("Album: ")
+
+def getID(title, artist):
+	searchPage = requests.get("https://www.allmusic.com/search/albums/" + artist.replace(" ", "+") + "+" + title.replace(" ", "+"), headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'})
+
+	searchSoup = BeautifulSoup(searchPage.content, "html.parser", parse_only=SoupStrainer("div", class_="title"))
+
+	#albumURL = searchSoup.find('a').get('href')
+
+	tooltip = searchSoup.find('a').get('data-tooltip')
+	id = re.search("[a-z]+\d+", tooltip)
+
+	return id[0]
+
+def getBasicInfo(title, artist):
+	searchPage = requests.get("https://www.allmusic.com/search/albums/" + artist.replace(" ", "+") + "+" + title.replace(" ", "+"), headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'})
+
+	idTitleSoup = BeautifulSoup(searchPage.content, "html.parser", parse_only=SoupStrainer("div", class_="title"))
+	artistSoup = BeautifulSoup(searchPage.content, "html.parser", parse_only=SoupStrainer("div", class_="artist"))
+
+	#albumURL = searchSoup.find('a').get('href')
+
+	tooltip = idTitleSoup.find('a').get('data-tooltip')
+	idCandidates = re.search("[a-z]+\d+", tooltip)
+
+	titleCandidate = idTitleSoup.find('a').text
+
+	artistCandidate = artistSoup.find('a').text
+
+
+	result = {'id':idCandidates[0], 'title':titleCandidate, 'artist':artistCandidate}
+	return result
+
+def getURL(id):
+	albumURL = f'https://www.allmusic.com/album/{id}'
+	return albumURL
+
+def getPage(url):
+
+	albumPage = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'})
+	return albumPage
+
+#******************************************d('t*****************************************************************************************
+
+def getMoods(id, db, moodModel):
+
+	albumSidebarSoup = BeautifulSoup(getPage(getURL(id)).content, "html.parser", parse_only=SoupStrainer("div", class_="sidebar"))
+
+	moodList = []
+
+	for count,mood in enumerate(albumSidebarSoup.find_all("span", class_="theme")):
+		
+		moodNameCandidate = mood.text.strip()
+		r = re.search("(([a-z]+\-)+)[a-z]+\d+", mood.a.get('href'))
+		moodIdCandidate = r[0]
+
+		# if the mood doesn't already exist in the Mood model in db, add it and commit.
+		if moodModel.query.filter_by(name=moodNameCandidate).one_or_none() == None:
+			db.session.add(moodModel(id=moodIdCandidate, name=moodNameCandidate))
+			db.session.commit()
+
+		moodList.append([moodNameCandidate, moodIdCandidate])
+
+	return moodList
+
+
+#***********************************************************************************************************************************
+
+def getSimilarAlbums(id):
+
+	similarAlbumPage = requests.get(getURL(id) + "/similar", headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'})	
+
+	similarAlbumSoup = BeautifulSoup(similarAlbumPage.content, "html.parser", parse_only=SoupStrainer("div", class_="album-highlights-container"))
+
+
+	similarAlbumList = []
+
+	for album in similarAlbumSoup.find_all("a"):
+		similarAlbumList.append([album.get('title'), album.get('href')])
+
+	print(similarAlbumList)
+
+#***********************************************************************************************************************************

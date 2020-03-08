841c8ea9752173f4fdecd2fbf9372c074837a7ad
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2020-03-07 23:44:23 -0500

    configuring for use with Cloud SQL

--- app.py
+++ app.py
@@ -28,7 +28,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SECRET_KEY'] = 'alpine'
 app.config['SQLALCHEMY_ECHO'] = False
 
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
+
+# The SQLAlchemy engine will help manage interactions, including automatically
+# managing a pool of connections to your database
+db = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # postgres+pg8000://<db_user>:<db_pass>@/<db_name>?unix_sock=/cloudsql/<cloud_sql_instance_name>/.s.PGSQL.5432
+    sqlalchemy.engine.url.URL(
+        drivername='postgres+pg8000',
+        username=db_user,
+        password=db_pass,
+        database=db_name,
+        query={
+            'unix_sock': '/cloudsql/{}/.s.PGSQL.5432'.format(
+                cloud_sql_connection_name)
+        }
+    ),
+    # ... Specify additional properties here.
+    # ...
+)
+
+
 admin = Admin(app)
 
 #****************************************************
